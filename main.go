81dafa86442df6bd03ddda0be9ec3056cccf66f8
keaising <keaising@gmail.com>
2021-08-27 12:23:45 +0800

    bugfix

--- main.go
+++ main.go
@@ -1,20 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/dghubble/oauth1"
 	"github.com/keaising/go-twitter/twitter"
 )
 
 func main() {
-	config := oauth1.NewConfig("", "")
-	token := oauth1.NewToken("", "")
+	config := oauth1.NewConfig(os.Getenv("CONSUMER_KEY"), os.Getenv("CONSUMER_SECRET"))
+	token := oauth1.NewToken(os.Getenv("TOKEN"), os.Getenv("TOKEN_SECRET"))
 	httpClient := config.Client(oauth1.NoContext, token)
 
 	// Twitter client
 	client := twitter.NewClient(httpClient)
-	var name = "Taiga"
+
+	prefix := os.Getenv("PREFIX")
+	loc, _ := time.LoadLocation("Asia/Shanghai")
+	days := int(time.Now().In(loc).Sub(time.Unix(1629003600, 0)).Hours() / 24)
+	var name = fmt.Sprintf("%s%v", prefix, days)
 	u, _, err := client.Accounts.UpdateProfile(&twitter.AccountUpdateProfileParams{
 		Name: &name,
 	})
